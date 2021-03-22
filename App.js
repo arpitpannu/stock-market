3feddab48426f13ae31ddca5114a02d3e3e940d4
arpitpannu <38281985+arpitpannu@users.noreply.github.com>
2021-03-22 14:53:31 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Home from "./components/Home/index";
+import Cards from "./components/Cards/index";
+import Table from "./components/Table/index";
+function App() {
+    return (
+        <div className="App">
+            <Home />
+            <Cards />
+            <Table />
+        </div>
+    );
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,113 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
+import ReactDOM from "react-dom";
 import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-class Table extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            stocks: [],
-            isFetching: false,
-            allStock: [
-                {
-                    companyName: "google",
-                    companySymbol: "+1cp",
-                    marketCap: "$145.4k",
-                    currentPrice: "$0.001",
-                },
-                {
-                    companyName: "facebook",
-                    companySymbol: "+1cp",
-                    marketCap: "$1775.4k",
-                    currentPrice: "$0.0301",
-                },
-                {
-                    companyName: "amazon",
-                    companySymbol: "+1hpp",
-                    marketCap: "$145.4k",
-                    currentPrice: "$0.001",
-                },
-                {
-                    companyName: "phonePay",
-                    companySymbol: "+1vcp",
-                    marketCap: "$145.4k",
-                    currentPrice: "$0.11001",
-                },
-            ],
-        };
-    }
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+);
 
-    // componentDidMount() {
-    //     const url =
-    //         "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=tesco&apikey=demo";
-    //     fetch(url)
-    //         .then((response) => response.json())
-    //         .then((result) => {
-    //             this.setState({
-    //                 stocks: result.bestMatches,
-    //                 isFetching: false,
-    //             });
-    //             console.log(result.bestMatches[0]);
-    //         })
-    //         .catch((e) => {
-    //             console.log(e);
-    //             this.setState({ ...this.state, isFetching: false });
-    //         });
-    // }
-    render() {
-        return (
-            <div className="container">
-                <div className="table">
-                    <div className="searchDiv">
-                        <label>Stock Details Table</label>
-                        <input
-                            type="text"
-                            placeholder="Search by Company Name"
-                        />
-                    </div>
-                    <div className="company-Detail-Table">
-                        <div className="table-header">
-                            <div className="companyName">Company Name</div>
-                            <div className="companySymbol">Company Symbol</div>
-                            <div className="marketCap">Market Cap</div>
-                            <div className="currentPrice">Current Price</div>
-                        </div>
-                        {/* {this.state.stocks.map((stock) => (
-                            <div className="details">
-                                <div className="companyNameText">
-                                    {stock.name}
-                                </div>
-                                <div className="companySymbolText">
-                                    {stock.symbol}
-                                </div>
-                                <div className="marketCapText">
-                                    {stock.matchScore}
-                                </div>
-                                <div className="currentPriceText">
-                                    {stock.currency}
-                                </div>
-                            </div>
-                        ))} */}
-
-                        {this.state.allStock.map((stock) => (
-                            <div className="details">
-                                <div className="companyNameText">
-                                    {stock.companyName}
-                                </div>
-                                <div className="companySymbolText">
-                                    {stock.companySymbol}
-                                </div>
-                                <div className="marketCapText">
-                                    {stock.marketCap}
-                                </div>
-                                <div className="currentPriceText">
-                                    {stock.currentPrice}
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
-export default Table;
+reportWebVitals();
